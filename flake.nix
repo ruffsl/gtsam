562981d1309826cd061a77312811f172a28385f7
ruffsl <roxfoxpox@gmail.com>
2026-01-20 21:55:51 -0600

    Add flake configuration and environment setup for GTSAM development

---
+++ flake.nix
@@ -0,0 +1,50 @@
+{
+  description = "GTSAM python development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs =
+    {
+      self,
+      nixpkgs,
+      flake-utils,
+    }:
+    flake-utils.lib.eachDefaultSystem (
+      system:
+      let
+        pkgs = import nixpkgs {
+          inherit system;
+          config = { allowUnfree = true; }; 
+        };
+        python = pkgs.python314.withPackages (ps: [
+          ps.graphviz
+          ps.jupyter
+          ps.matplotlib
+          ps.numpy
+          ps.pybind11-stubgen
+          ps.pyparsing
+        ]);
+      in
+      {
+        devShell = pkgs.mkShell {
+          packages = with pkgs; [
+            ccache
+            cmake
+            cmakeWithGui
+            boost
+            tbb
+            mkl
+            eigen
+            lldb
+            python
+          ];
+          shellHook = ''
+            export PYTHONPATH="$PWD/build/python''${PYTHONPATH:+:$PYTHONPATH}"
+          '';
+        };
+      }
+    );
+}
